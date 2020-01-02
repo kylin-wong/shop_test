aedf692a5c702baba5fe2a748982a7491ec9517d
Levi <349345860@qq.com>
2020-01-02 13:22:22 +0800

    Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../components/Home/home.vue'
+import Member from '../components/Member/member.vue'
+import Shopcar from '../components/Shopcar/shopcar.vue'
+import Search from '../components/Search/search.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/member',
+    component: Member
+  },
+  {
+    path: '/shopcar',
+    component: Shopcar
+  },
+  {
+    path: '/search',
+    component: Search
+  }
+]
 
 const router = new VueRouter({
   routes
