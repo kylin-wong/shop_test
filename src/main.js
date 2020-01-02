256268b36c7a6715617d930e1363c0dfeb64e1db
shan <1132649527@qq.con>
2020-01-02 11:41:07 +0800

    添加vant.js 和字体图标

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
+//导入vant.js的按需组件的信息
+//导入vant.js里面的组件
+import './plugins/vant.js'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//导入字体图标
+import './assets/font/iconfont.css'
 
 Vue.config.productionTip = false
 
