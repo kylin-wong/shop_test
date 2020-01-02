c6c34169af639ab9a2aa0ac538be748a46127b7c
Levi <349345860@qq.com>
2020-01-02 16:18:28 +0800

    shopCar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 // 导入vant.js里面的组件
 import './plugins/vant.js'
 
+// 导入axios
+import axios from 'axios'
+
 // APP根文件 Router Vuex
 import App from './App.vue'
 import router from './router'
@@ -18,6 +21,25 @@ import myComment from './components/myCommon/myComment.vue'
 Vue.component('my-header', myHeader)
 Vue.component('my-footer', myFooter)
 Vue.component('my-comment', myComment)
+
+// 整数转精度2位
+Vue.filter('my-filter', function returnFloat(value) {
+  var val = Math.round(parseFloat(value) * 100) / 100
+  var xsd = value.toString().split('.')
+  if (xsd.length === 1) {
+    value = value.toString() + '.00'
+    return value
+  }
+  if (xsd.length > 1) {
+    if (xsd[1].length < 2) {
+      value = value.toString() + '0'
+    }
+    return val
+  }
+})
+// axios 配置
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,9 +1,17 @@
 // 导入vue
 import Vue from 'vue'
-import { Divider, Button, Row, Col, Search, Tag } from 'vant'
+import { SubmitBar, Sticky, Dialog, Divider, Button, Row, Col, Search, Tag, Card, Notify } from 'vant'
 Vue.use(Button)
 Vue.use(Row)
 Vue.use(Col)
 Vue.use(Tag)
 Vue.use(Search)
 Vue.use(Divider)
+Vue.use(Card)
+Vue.use(Notify)
+Vue.use(SubmitBar)
+Vue.use(Sticky)
+
+Vue.use(Dialog)
+Vue.prototype.Dialog = Dialog
+Vue.prototype.$message = Notify
