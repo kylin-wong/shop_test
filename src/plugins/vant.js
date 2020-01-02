08af743b4f2382fd665756d44afdcbbd9ab584ac
Levi <349345860@qq.com>
2020-01-02 14:41:06 +0800

    search

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,4 +1,9 @@
 // 导入vue
 import Vue from 'vue'
-import { Button } from 'vant'
+import { Divider, Button, Row, Col, Search, Tag } from 'vant'
 Vue.use(Button)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Tag)
+Vue.use(Search)
+Vue.use(Divider)
