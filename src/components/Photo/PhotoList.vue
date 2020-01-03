7694c216599b71a3a04688d5b23d2e8cc9073e46
kylin-wong <375506637@qq.com>
2020-01-03 15:14:05 +0800

    photolist optimize

--- src/components/Photo/PhotoList.vue
+++ src/components/Photo/PhotoList.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
     <!-- 标签页 -->
-    <van-tabs line-width="40px" swipeable :duration="0.2" v-model="activeName" @change="getImgList">
-      <van-tab v-for="item in ImgCategory" :title="item.title" :key="item.id"> </van-tab>
+    <van-tabs
+      line-width="40px"
+      swipeable
+      :duration="0.2"
+      v-model="activeName"
+      @change="getImgList"
+    >
+      <van-tab v-for="item in ImgCategory" :title="item.title" :key="item.id">
+      </van-tab>
     </van-tabs>
     <!-- 全部图片区域 -->
     <ul class="img_list">
       <!-- 将图片数组中的数据遍历渲染到页面 -->
-      <li v-for="item in getImges" :key="item.id" class="box" @click="jumpImg(item.id)">
-        <van-image width="100%" height="300px" :src="item.img_url" lazy-load fit="cover" radius="5px">
+      <li
+        v-for="item in getImges"
+        :key="item.id"
+        class="box"
+        @click="jumpImg(item.id)"
+      >
+        <van-image
+          width="100%"
+          height="300px"
+          :src="item.img_url"
+          lazy-load
+          fit="cover"
+          radius="5px"
+        >
           <template>
             <van-loading type="spinner" color="#1989fa" />
           </template>
@@ -51,11 +70,17 @@ export default {
       this.ImgCategory = res.message
       // console.log(this.ImgCategory)
     },
+    // 获取图片信息
     async getImages(id) {
       const { data: res } = await this.$http.get(`/api/getimages/${this.id}`)
       // 将获取的图片数据放图数组中
       this.getImges = res.message
-      // console.log(res.message)
+      //   console.log(res.message)
+      res.message.forEach(item => {
+        var abst = item.zhaiyao.substring(0, 30)
+        var abstInfo = abst + (item.zhaiyao.length > 30 ? '……' : '')
+        item.zhaiyao = abstInfo
+      })
     },
     // tab栏发生改变时触发
     getImgList(index) {
@@ -72,22 +97,25 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.img_list {
-  padding: 0 10px;
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
-.img_list .li {
-  display: flex;
-  align-items: center;
+.img_list {
+  padding: 0 10px 50px 10px;
+}
+
+.box {
+  position: relative;
+  margin: 10px;
   width: 100%;
+  margin: 10px 0;
   border-radius: 5px;
   overflow: hidden;
   height: 300px;
   min-height: 200px;
   position: relative;
-}
-.box {
-  position: relative;
-  margin: 10px;
 }
 
 .botInfo {
@@ -97,12 +125,11 @@ export default {
   width: 100%;
   background: rgba(0, 0, 0, 0.4);
   color: #fff;
-  height: 75px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
 }
 .botInfo p {
-  margin-top: 1px;
-  margin-left: 10px;
-  margin-bottom: 5px;
+  margin: 5px 10px;
   font-size: 12px;
 }
 </style>
