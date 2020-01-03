6ddb5df4001e6803cdae1948df545e8d2b4bfd33
kylin-wong <375506637@qq.com>
2020-01-03 16:36:07 +0800

    init

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -20,27 +20,16 @@ import {
   SubmitBar,
   Sticky,
   Dialog,
-  Divider,
-  Button,
-  Row,
-  Col,
-  Search,
   Tag,
   Card,
   Notify,
   Tab,
   Tabs,
-  Lazyload,
-  Image,
-  Tabbar,
-  Icon,
   TabbarItem,
   NavBar,
   Loading,
   PullRefresh,
-  TabbarItem,
-  NavBar,
-  Loading
+  Toast
 } from 'vant'
 
 Vue.use(Grid)
@@ -57,11 +46,7 @@ Vue.use(CellGroup)
 Vue.use(Cell)
 Vue.use(Button)
 Vue.use(Row)
-Vue.use(Col)
 Vue.use(Tag)
-Vue.use(Search)
-Vue.use(Divider)
-Vue.use(Tabbar)
 Vue.use(Icon)
 Vue.use(TabbarItem)
 Vue.use(NavBar)
@@ -79,7 +64,4 @@ Vue.use(Loading)
 Vue.prototype.Dialog = Dialog
 Vue.prototype.$message = Notify
 Vue.use(Tab).use(Tabs)
-Vue.use(Lazyload)
-Vue.use(Image)
 Vue.use(Notify)
-Vue.use(Loading)
