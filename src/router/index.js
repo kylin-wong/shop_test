96c18e3d8ad318709a7572b8f795bb933962d4e4
Marshu668 <869715017@qq.com>
2020-01-03 11:03:39 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../components/Home/home.vue'
+import Home from '../components/Home/Home.vue'
 import Member from '../components/Member/member.vue'
 import Shopcar from '../components/Shopcar/shopcar.vue'
 import Search from '../components/Search/search.vue'
